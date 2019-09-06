10fed3b1673bcb8b87bc5cc578b22550866dc470
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-09-05 20:13:05 -0500

    latest set of changes.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include <emscripten.h>
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
 #include "protocol.h"
 #include "screen.h"
 #include "io.h"
@@ -13,6 +14,9 @@ unsigned char already_started=false;
 SDL_Event event;
 int something_happened;
 
+static double _emsc_width = 0;
+static double _emsc_height = 0;
+
 extern unsigned char DoNotSend;
 
 void loop(void)
@@ -23,6 +27,13 @@ void loop(void)
   screen_main();
 }
 
+/* track CSS element size changes and update the WebGL canvas size */
+static EM_BOOL _emsc_size_changed(int event_type, const EmscriptenUiEvent* ui_event, void* user_data) {
+  emscripten_get_element_css_size("#canvas", &_emsc_width, &_emsc_height);
+  emscripten_set_canvas_element_size("#canvas", _emsc_width, _emsc_height);
+  return true;
+}
+
 int main(void)
 {
   screen_init();
@@ -31,5 +42,8 @@ int main(void)
   terminal_initial_position();
   io_init();
   DoNotSend=0;
+  emscripten_get_element_css_size("#canvas", &_emsc_width, &_emsc_height);
+  emscripten_set_canvas_element_size("#canvas", _emsc_width, _emsc_height);
+  emscripten_set_resize_callback(0, 0, false, _emsc_size_changed);
   emscripten_set_main_loop(loop,0,0);
 }
